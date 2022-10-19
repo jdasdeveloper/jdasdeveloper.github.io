44bf8c22d2290b174601a3e45230752ad1f52d61
jdasdeveloper <100498503+jdasdeveloper@users.noreply.github.com>
2022-10-19 14:09:28 -0400

    Add files via upload

--- medidove/js/header.js
+++ medidove/js/header.js
@@ -9,10 +9,7 @@ function resizeViewport() {
         {
             document.getElementById("header_logo").style.backgroundImage = "url('./images/logo1.png')";
             document.getElementById("navbar").style.backdropFilter = "blur(10px)";
-            
         }
-            // document.getElementById("navbar").style.backdropFilter = "blur(10px)";
-            // document.getElementById("navbar").style.background = "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)";
             
     }
     else if (document.body.scrollTop < 45 || document.documentElement.scrollTop < 45)
@@ -20,14 +17,11 @@ function resizeViewport() {
         document.getElementById("navbar").style.backdropFilter = "blur(0)";
         document.getElementById("navbar").style.background = "linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.0))";
         document.getElementById("header_logo").style.backgroundImage = "url('./images/logo-blanco.png')";
-        // reset("#f3f6ff");
     }
     else{
         
     }
 }
-// resizeViewport();
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function () {
     scrollFunction();
 };
@@ -36,23 +30,18 @@ function scrollFunction() {
     if (document.documentElement.clientWidth >= 1010){
         if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0)
         {
-           
             document.getElementById("navbar").style.backdropFilter = "blur(10px)";
             document.getElementById("navbar").style.webkitBackdropFilter = "blur(10px)";
-            // document.getElementById("navbar").style.background = "linear-gradient(rgba(243, 246, 255, 0.5), rgba(243, 246, 255, 0.5)";
             document.getElementById("header_logo").style.backgroundImage = "url('./images/logo1.png')";
-            // reset("rgb(5,5,5)");
         }
         else if (document.body.scrollTop <= 45 || document.documentElement.scrollTop <= 45)
         {
             $("#navbar").animate({
                 background: "linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.0))",
             }, 1000 );
-            // document.getElementById("navbar").style.background = "linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.0))";
             document.getElementById("navbar").style.backdropFilter = "blur(0)";
             document.getElementById("navbar").style.webkitBackdropFilter = "blur(0px)";
             document.getElementById("header_logo").style.backgroundImage = "url('./images/logo-blanco.png')";
-            // reset("#f3f6ff");
         }
 }
 else{
@@ -73,19 +62,6 @@ visualViewport.onresize = () => {
     resizeViewport();
 };
 
-function reset(color) {
-	var elements = document.getElementsByClassName('header-option'); // get all elements
-	for(var i = 0; i < elements.length; i++){
-		elements[i].style.color = color;
-	}
-}
-
-// document.addEventListener('load', (event) => {
-//     console.log('page is fully loaded');
-//     if (document.documentElement.clientWidth <= 1010){
-//         document.getElementById("header_logo").style.backgroundImage = "url('./images/logo-blanco.png')";
-//     }
-//   });
   $(document).ready(function(){
     console.log('page is fully loaded');
     if (document.documentElement.clientWidth <= 1010){
