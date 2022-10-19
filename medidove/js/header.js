8eb8445042433bd0645c4d4b65bad2d705703a67
jdasdeveloper <100498503+jdasdeveloper@users.noreply.github.com>
2022-10-18 21:06:45 -0400

    Add files via upload

--- medidove/js/header.js
+++ medidove/js/header.js
@@ -2,25 +2,28 @@ function resizeViewport() {
     if (document.documentElement.clientWidth <= 992)
     {
         if (document.body.scrollTop < 1 || document.documentElement.scrollTop < 1)
-    {  document.getElementById("navbar").style.backdropFilter = "blur(2px)"; }
-    else{
-        document.getElementById("header_logo").style.backgroundImage = "url('./images/logo1.png')";
-        document.getElementById("navbar").style.backdropFilter = "blur(10px)";
-        reset("rgba(0, 0, 0, 0.5)");
-    }
-        // document.getElementById("navbar").style.backdropFilter = "blur(10px)";
-        // document.getElementById("navbar").style.background = "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)";
-        
+        {  
+            document.getElementById("navbar").style.backdropFilter = "blur(2px)"; 
+        }
+        else
+        {
+            document.getElementById("header_logo").style.backgroundImage = "url('./images/logo1.png')";
+            document.getElementById("navbar").style.backdropFilter = "blur(10px)";
+            
+        }
+            // document.getElementById("navbar").style.backdropFilter = "blur(10px)";
+            // document.getElementById("navbar").style.background = "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)";
+            
     }
     else if (document.body.scrollTop < 45 || document.documentElement.scrollTop < 45)
     {   
         document.getElementById("navbar").style.backdropFilter = "blur(0)";
         document.getElementById("navbar").style.background = "linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.0))";
         document.getElementById("header_logo").style.backgroundImage = "url('./images/logo-blanco.png')";
-        reset("#f3f6ff");
+        // reset("#f3f6ff");
     }
     else{
-      
+        
     }
 }
 // resizeViewport();
@@ -49,7 +52,7 @@ function scrollFunction() {
             document.getElementById("navbar").style.backdropFilter = "blur(0)";
             document.getElementById("navbar").style.webkitBackdropFilter = "blur(0px)";
             document.getElementById("header_logo").style.backgroundImage = "url('./images/logo-blanco.png')";
-            reset("#f3f6ff");
+            // reset("#f3f6ff");
         }
 }
 else{
